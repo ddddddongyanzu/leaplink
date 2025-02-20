c791206da1dad911b6e3280591a4a2e242fc10cd
yanzu_dong <1774972617@qq.com>
2025-02-20 13:50:04 +0800

    feature: 开发短链接规约功能

---
+++ admin/src/main/java/com/archaist/leaplink_demo/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,14 @@
+package com.archaist.leaplink_demo.admin.common.convention.errorcode;
+
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
