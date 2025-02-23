eeedb1c46249d6cb633afa0b11433618e53e7aea
yanzu_dong <1774972617@qq.com>
2025-02-23 15:55:15 +0800

    fixed: 短链接创建参数bug

--- admin/src/main/java/com/archaist/leaplink_demo/admin/remote/dto/req/ShortLinkCreateReqDTO.java
+++ admin/src/main/java/com/archaist/leaplink_demo/admin/remote/dto/req/ShortLinkCreateReqDTO.java
@@ -1,4 +1,4 @@
-package com.archaist.leaplink_demo.project.dto.req;
+package com.archaist.leaplink_demo.admin.remote.dto.req;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
