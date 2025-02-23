751f4b853327aedd927a8c984c389971a3641d02
yanzu_dong <1774972617@qq.com>
2025-02-23 15:55:35 +0800

    fixed: 短链接后管系统请求中台服务接口

---
+++ admin/src/main/java/com/archaist/leaplink_demo/admin/controller/ShortLinkController.java
@@ -0,0 +1,41 @@
+package com.archaist.leaplink_demo.admin.controller;
+
+
+import com.archaist.leaplink_demo.admin.common.convention.result.Result;
+import com.archaist.leaplink_demo.admin.remote.dto.ShortLinkRemoteService;
+import com.archaist.leaplink_demo.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.archaist.leaplink_demo.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.archaist.leaplink_demo.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接后管控制层
+ */
+@RestController
+public class ShortLinkController {
+
+    /**
+     * 后续重构为 SpringCloud Feign 调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {};
+
+    /**
+     * 创建短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/create")
+    public Result<ShortLinkPageRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
+        return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 分页查询短链接
+     */
+    @GetMapping("/api/short-link/admin/v1/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return shortLinkRemoteService.pageShortLink(requestParam);
+    }
+}
