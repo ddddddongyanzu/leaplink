5d4ce69d5415cbad573e01dd502f419ce99a49b4
yanzu_dong <1774972617@qq.com>
2025-02-26 18:35:45 +0800

    refactor: 重构短链接不存在跳转提示页面

---
+++ project/src/main/java/com/archaist/leaplink_demo/project/controller/ShortLinkNotFoundController.java
@@ -0,0 +1,19 @@
+package com.archaist.leaplink_demo.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 短链接不存在跳转控制器
+ */
+@Controller
+public class ShortLinkNotFoundController {
+
+    /**
+     * 短链接不存在跳转页面
+     */
+    @RequestMapping("/page/notfound")
+    public String notfound() {
+        return "notfound";
+    }
+}
