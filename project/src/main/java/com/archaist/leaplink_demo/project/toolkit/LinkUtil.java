33482d6c7541845fd33f8703a4813b17dd1ef32f
yanzu_dong <1774972617@qq.com>
2025-03-04 10:12:08 +0800

    feature: 开发短链接基础访问uip访问统计

--- project/src/main/java/com/archaist/leaplink_demo/project/toolkit/LinkUtil.java
+++ project/src/main/java/com/archaist/leaplink_demo/project/toolkit/LinkUtil.java
@@ -3,6 +3,7 @@ package com.archaist.leaplink_demo.project.toolkit;
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
 import com.archaist.leaplink_demo.project.common.constant.ShortLinkConstant;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Date;
 import java.util.Optional;
@@ -22,5 +23,34 @@ public class LinkUtil {
         return Optional.ofNullable(validDate)
                 .map(each -> DateUtil.between(new Date(), each, DateUnit.MS))
                 .orElse(ShortLinkConstant.DEFAULT_CACHE_VALID_TIME);
+    }
+
+    /**
+     * 获取用户真实ip
+     * @param request 请求
+     * @return 用户真实ip
+     */
+    public static String getActualIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        // 如果有多个 IP（例如：经过多个代理），取第一个有效的 IP
+        if (ip != null && ip.contains(",")) {
+            ip = ip.split(",")[0].trim();
+        }
+        return ip;
     }
 }
