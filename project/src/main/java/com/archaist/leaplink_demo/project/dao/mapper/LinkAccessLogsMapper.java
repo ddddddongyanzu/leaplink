cb5f3e2bde79a61ae862441b7b75f1f5b3ca19f7
yanzu_dong <1774972617@qq.com>
2025-03-05 10:44:41 +0800

    feature: 开发短链接监控高频访问之IP功能

---
+++ project/src/main/java/com/archaist/leaplink_demo/project/dao/mapper/LinkAccessLogsMapper.java
@@ -0,0 +1,10 @@
+package com.archaist.leaplink_demo.project.dao.mapper;
+
+import com.archaist.leaplink_demo.project.dao.entity.LinkAccessLogsDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 访问日志监控持久层
+ */
+public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
+}
