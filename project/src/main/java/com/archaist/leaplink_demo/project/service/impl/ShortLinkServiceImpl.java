9bb13b32404b09acaf95f007b33a711ec4bd9e69
yanzu_dong <1774972617@qq.com>
2025-03-05 11:08:05 +0800

    feature: 开发短链接监控之网络信息

--- project/src/main/java/com/archaist/leaplink_demo/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/archaist/leaplink_demo/project/service/impl/ShortLinkServiceImpl.java
@@ -77,6 +77,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
     private final LinkBrowserStatsMapper linkBrowserStatsMapper;
     private final LinkAccessLogsMapper linkAccessLogsMapper;
     private final LinkDeviceStatsMapper linkDeviceStatsMapper;
+    private final LinkNetworkStatsMapper linkNetworkStatsMapper;
 
     @Value("${short-link.stats.local.amap-key}")
     private String staticLocalAmapKey;
@@ -368,6 +369,14 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                         .date(new Date())
                         .build();
                 linkDeviceStatsMapper.shortLinkDeviceState(linkDeviceStatsDO);
+                LinkNetworkStatsDO linkNetworkStatsDO = LinkNetworkStatsDO.builder()
+                        .network(LinkUtil.getNetwork(((HttpServletRequest) request)))
+                        .cnt(1)
+                        .gid(gid)
+                        .fullShortUrl(fullShortUrl)
+                        .date(new Date())
+                        .build();
+                linkNetworkStatsMapper.shortLinkNetworkState(linkNetworkStatsDO);
             }
         } catch (Throwable ex) {
             log.error("短链接统计量异常", ex);
