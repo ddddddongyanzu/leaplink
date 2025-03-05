527e853e04a2372958c782ce7f5d2a3b63d25116
yanzu_dong <1774972617@qq.com>
2025-03-05 10:59:40 +0800

    feature: 开发短链接监控之访问设备信息

---
+++ project/src/main/java/com/archaist/leaplink_demo/project/dao/entity/LinkDeviceStatsDO.java
@@ -0,0 +1,51 @@
+package com.archaist.leaplink_demo.project.dao.entity;
+
+import com.archaist.leaplink_demo.project.common.database.BaseDO;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 访问设备统计访问实体
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_link_device_stats")
+public class LinkDeviceStatsDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 浏览器
+     */
+    private String device;
+}
