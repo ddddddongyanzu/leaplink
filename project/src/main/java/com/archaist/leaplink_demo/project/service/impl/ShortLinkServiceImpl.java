c4e68c789e428c981574ba54554beaa03674dbaf
yanzu_dong <1774972617@qq.com>
2025-03-06 10:09:42 +0800

    feature: 开发短链接新增时初始化访问为0

--- project/src/main/java/com/archaist/leaplink_demo/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/archaist/leaplink_demo/project/service/impl/ShortLinkServiceImpl.java
@@ -99,6 +99,9 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 .describe(requestParam.getDescribe())
                 .shortUri(shortLinkSuffix)
                 .enableStatus(0)
+                .totalPv(0)
+                .totalUv(0)
+                .totalUip(0)
                 .fullShortUrl(fullShortUrl)
                 .favicon(getFavicon(requestParam.getOriginUrl()))
                 .build();
@@ -383,6 +386,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                         .fullShortUrl(fullShortUrl)
                         .build();
                 linkAccessLogsMapper.insert(linkAccessLogsDO);
+                baseMapper.incrementStats(gid, fullShortUrl, 1, uvFirstFlag.get() ? 1 : 0, uipFirstFlag ? 1 : 0);
             }
         } catch (Throwable ex) {
             log.error("短链接统计量异常", ex);
