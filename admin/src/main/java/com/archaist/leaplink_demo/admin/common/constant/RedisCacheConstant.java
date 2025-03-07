2955fdfdabf3ded58abb27ebc934025aa6e294cb
yanzu_dong <1774972617@qq.com>
2025-03-07 20:51:10 +0800

    optimize: 优化单个用户分组数量不能大于20

--- admin/src/main/java/com/archaist/leaplink_demo/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/archaist/leaplink_demo/admin/common/constant/RedisCacheConstant.java
@@ -4,5 +4,14 @@ package com.archaist.leaplink_demo.admin.common.constant;
  * 短链接后管 Redis 缓存常量
  */
 public class RedisCacheConstant {
+
+    /**
+     * 用户注册分布式锁
+     */
     public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register";
+
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create: %s";
 }
