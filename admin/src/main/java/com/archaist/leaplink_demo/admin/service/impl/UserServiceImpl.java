2dc04646e61cf62df6885cb062786d0ab768c390
yanzu_dong <1774972617@qq.com>
2025-03-07 20:31:32 +0800

    refactor: 用户已登录状态如果重复登录返回Token

--- admin/src/main/java/com/archaist/leaplink_demo/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/archaist/leaplink_demo/admin/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.archaist.leaplink_demo.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.alibaba.fastjson2.JSON;
 import com.archaist.leaplink_demo.admin.common.constant.RedisCacheConstant;
 import com.archaist.leaplink_demo.admin.common.convention.exception.ClientException;
@@ -26,6 +27,7 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -103,9 +105,13 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (userDO == null) {
             throw new ClientException("用户不存在");
         }
-        Boolean hasKey = stringRedisTemplate.hasKey("login_" + requestParam.getUsername());
-        if (hasKey != null && hasKey) {
-            throw new ClientException("用户已登录");
+        Map<Object, Object> hasLoginMap = stringRedisTemplate.opsForHash().entries("login_" + requestParam.getUsername());
+        if (CollUtil.isNotEmpty(hasLoginMap)) {
+            String token = hasLoginMap.keySet().stream()
+                    .findFirst()
+                    .map(Object::toString)
+                    .orElseThrow(() -> new ClientException("用户登录错误"));
+            return new UserLoginRespDTO(token);
         }
         /**
          * Hash
