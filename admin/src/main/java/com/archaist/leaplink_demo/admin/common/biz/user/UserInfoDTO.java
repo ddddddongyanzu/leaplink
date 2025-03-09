c9f1113ad91e8d9b91fa3e45eb6bf773b83f28a3
yanzu_dong <1774972617@qq.com>
2025-03-09 19:47:38 +0800

    feature: 引入网关改造短链接服务

--- admin/src/main/java/com/archaist/leaplink_demo/admin/common/biz/user/UserInfoDTO.java
+++ admin/src/main/java/com/archaist/leaplink_demo/admin/common/biz/user/UserInfoDTO.java
@@ -31,8 +31,4 @@ public class UserInfoDTO {
      */
     private String realName;
 
-    /**
-     * 用户 Token
-     */
-    private String token;
 }
--- admin/src/main/java/com/archaist/leaplink_demo/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/archaist/leaplink_demo/admin/common/enums/UserErrorCodeEnum.java
@@ -4,8 +4,6 @@ import com.archaist.leaplink_demo.admin.common.convention.errorcode.IErrorCode;
 
 public enum UserErrorCodeEnum implements IErrorCode {
 
-    USER_TOKEN_FAIL("A00200", "用户Token验证失败"),
-
     USER_NULL("B000200", "用户记录不存在"),
 
     USER_NAME_EXIST("B000201", "用户名已存在"),
